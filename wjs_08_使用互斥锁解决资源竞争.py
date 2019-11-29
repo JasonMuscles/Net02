c5b24906e3244c4cfc4b25daf99c266d16dcd0aa
JasonMuscles <wangjinsenufo@qq.com>
2019-11-29 17:24:46 +0800

    线程 共享全局变量  args 锁

---
+++ wjs_08_使用互斥锁解决资源竞争.py
@@ -0,0 +1,50 @@
+import threading
+import time
+
+# 定义一个全局变量
+g_num = 0
+
+
+def test1(num):
+    global g_num
+    # 上锁，之前没有上锁此刻上锁成功
+    # 如之前已上锁，那么到此时会阻塞在此，直到锁被解开为止
+    mutex.acquire()
+    for i in range(num):
+        g_num += 1
+    # 解锁
+    mutex.release()
+    print("—————test1 g_num=%s—————" % g_num)
+
+
+def test2(num):
+    global g_num
+    mutex.acquire()
+    for i in range(num):
+        g_num += 1
+    mutex.release()
+    print("—————test2 g_num=%s—————" % g_num)
+
+
+# 创建一个互斥锁（默认没有上锁）
+mutex = threading.Lock()
+
+
+def main():
+    # target指定将来这个线程去哪个函数执行代码
+    # args自定将来调用函数的时候传递什么数据过去
+
+    t1 = threading.Thread(target=test1, args=(100000000, ))
+    t2 = threading.Thread(target=test2, args=(100000000, ))
+
+    t1.start()
+    t2.start()
+
+    # 等待上面执行完成
+    time.sleep(15)
+
+    print("----in main Thread g_nums = %s-----" % g_num)
+
+
+if __name__ == '__main__':
+    main()
